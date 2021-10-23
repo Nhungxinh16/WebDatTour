96f7e4d59c4fe625bb02cab18565b27e10e4a19f
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2021-10-23 17:41:44 +0700

    header va footer

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
     require("config/constants.php");
 ?>
 
+<!-- Ảnh bìa -->
 
+<img src="images/sl_211018_du-lich-an-toan.jpg" class="img-fluid" style="width: 100%;" alt="Responsive image">
+
+<hr>
 
 <?php
     include('templates/footer.php')
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,86 @@
 <!DOCTYPE html>
-<html lang = "en">
-    <head>
-        <meta charset="UTP-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Danh Bạ Điện Tử</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-    </head>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+  <link href="css/bootstrap.css" media="all" type="text/css" rel="stylesheet">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js" type="text/javascript"></script>
+  <title>Web đặt tour</title>
+</head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light p-1 pb-0 pt-0 ">
+    <a class="navbar-brand m-0" href="#"><img src="images/logo.png" class="img-fluid ml-2" alt="Responsive image" style="max-width: 70%"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+
+        <li class="nav-item dropdown active p-2">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-weight: 500; font-size:small;">
+            Du lịch
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item p-3" href="#">Du lịch trong nước</a>
+            <a class="dropdown-item p-3" href="#">Du lịch nước ngoài</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown active p-2">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-weight: 500; font-size:small;">
+            Vận chuyển
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item p-3" href="#">Thuê xe</a>
+            <a class="dropdown-item p-3" href="#">Vé máy bay</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown active p-2">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-weight: 500; font-size:small;">
+            Tin tức
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item p-3" href="#">Tin mới</a>
+            <a class="dropdown-item p-3" href="#">Tin tức du lịch</a>
+            <a class="dropdown-item p-3" href="#">Cẩm nang du lịch</a>
+            <a class="dropdown-item p-3" href="#">Kinh nghiệm du lịch</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown active p-2">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="font-weight: 500; font-size:small;">
+            Khuyến mãi
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item p-3" href="#">Mua tour trả chậm 0%</a>
+            <a class="dropdown-item p-3" href="#">Ưu đãi cùng VIB</a>
+            <a class="dropdown-item p-3" href="#">Ưu đãi đổi VNpay</a>
+            <a class="dropdown-item p-3" href="#">Hoàn tiền MSB</a>
+            <a class="dropdown-item p-3" href="#">Hoàn tiền BIDV</a>
+          </div>
+        </li>
+
+        <li class="nav-item active p-2" id="navbarDropdown">
+          <a class="nav-link disabled" style="font-weight: 500; font-size:small;" href="#">VietravelPlus</a>
+        </li>
+
+        <li class="nav-item active p-2 " id="navbarDropdown">
+          <a class="nav-link disabled" style="font-weight: 500; font-size:small;" href="#">Liên hệ</a>
+        </li>
+
+      </ul>
+
+<!--  Tìm kiếm trong thanh menu  -->
+
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-1 sm-2 border-warning" type="search" placeholder="Bắt đầu tìm kiếm..." aria-label="Search">
+        <button class="btn btn-outline my-2 my-sm-0 mr-5" type="submit"><i class="fas fa-search"></i></button>
+      </form>
+    </div>
+  </nav>
