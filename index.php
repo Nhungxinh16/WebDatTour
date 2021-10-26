823d0c87defca7d6642d214372003e7c7ae21b74
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2021-10-26 09:31:14 +0700

    them chuc nang cac tour yeu thich

--- index.php
+++ index.php
@@ -128,61 +128,39 @@
     </div>
 
     
-    <p class="m-2" style="color:#282365; font-size:xx-large; font-weight: 600;">Tour gia đình</p>
+    <p class="m-2" style="color:#282365; font-size:xx-large; font-weight: 600;">Các tour được yêu thích</p>
 
     <div class="card-deck">
-      <div class="card uu-dai">
-        <a href="#"><img src="content/image/giadinh/giadinh1.jpg" class="card-img-top" alt="..."></a>
-        <div class="card-body">
-          <p class="card-text mb-1"><small class="text-muted">28/10/2021 - Trong ngày - Giờ đi: 08:00</small></p>
-          <a href="#"><h5 class="card-title pr-2 mb-1" style="color: #282365; font-size: medium;">Hành trình xanh Cần Giờ (Tàu cao
-            tốc) - Khám phá KDL Sinh thái Dần Xây - Vàm Sát (Nhóm 6 khách)</h5></a>
-          <p class="card-text mb-0"><small class="text-muted">Mã tour:</small></p>
-          <p class="card-text mb-2"><small class="text-muted"><i class="fas fa-ticket"></i>
-              NDSGN8286-001-281021XE-V</small></p>
-          <p class="card-text"><small class="text-muted">Nơi khởi hành TP.Hồ Chí Minh</small></p>
-          <p class="card-text"><b style="font-size: large; color:#FD5056;">17,940,000đ</b><small class="text-muted">/gia
-              đình 6 khách</small></p>
-          <a href="#" class="btn btn-danger" style="font-size: small; background-color: #D74449;"><i
-              class="fas fa-shopping-cart"></i>&#160;Đặt ngay</a>
-          <a href="#" class="btn btn-light" style="font-size: small; background-color: #ffffff; float: right; border: 1px solid #2f24a7; color: #2f24a7;">Xem chi tiết</a>
-
-        </div>
-      </div>
-      <div class="card uu-dai">
-        <a href="#"><img src="content/image/giadinh/giadinh1.jpg" class="card-img-top" alt="..."></a>
-        <div class="card-body">
-          <p class="card-text mb-1"><small class="text-muted">28/10/2021 - Trong ngày - Giờ đi: 08:00</small></p>
-          <a href="#"><h5 class="card-title pr-2 mb-1" style="color: #282365; font-size: medium;">Hành trình xanh Cần Giờ (Tàu cao
-            tốc) - Khám phá KDL Sinh thái Dần Xây - Vàm Sát (Nhóm 6 khách)</h5></a>
-          <p class="card-text mb-0"><small class="text-muted">Mã tour:</small></p>
-          <p class="card-text mb-2"><small class="text-muted"><i class="fas fa-ticket"></i>
-              NDSGN8286-001-281021XE-V</small></p>
-          <p class="card-text"><small class="text-muted">Nơi khởi hành TP.Hồ Chí Minh</small></p>
-          <p class="card-text"><b style="font-size: large; color:#FD5056;">17,940,000đ</b><small class="text-muted">/gia
-              đình 6 khách</small></p>
-          <a href="#" class="btn btn-danger" style="font-size: small; background-color: #D74449;"><i
-              class="fas fa-shopping-cart"></i>&#160;Đặt ngay</a>
-          <a href="#" class="btn btn-light" style="font-size: small; background-color: #ffffff; float: right; border: 1px solid #2f24a7; color: #2f24a7;">Xem chi tiết</a>
-        </div>
-      </div>
-      <div class="card uu-dai">
-        <a href="#"><img src="content/image/giadinh/giadinh1.jpg" class="card-img-top" alt="..."></a>
-        <div class="card-body">
-          <p class="card-text mb-1"><small class="text-muted">28/10/2021 - Trong ngày - Giờ đi: 08:00</small></p>
-          <a href="#"><h5 class="card-title pr-2 mb-1" style="color: #282365; font-size: medium;">Hành trình xanh Cần Giờ (Tàu cao
-            tốc) - Khám phá KDL Sinh thái Dần Xây - Vàm Sát (Nhóm 6 khách)</h5></a>
-          <p class="card-text mb-0"><small class="text-muted">Mã tour:</small></p>
-          <p class="card-text mb-2"><small class="text-muted"><i class="fas fa-ticket"></i>
-              NDSGN8286-001-281021XE-V</small></p>
-          <p class="card-text"><small class="text-muted">Nơi khởi hành TP.Hồ Chí Minh</small></p>
-          <p class="card-text"><b style="font-size: large; color:#FD5056;">17,940,000đ</b><small class="text-muted">/gia
-              đình 6 khách</small></p>
-          <a href="#" class="btn btn-danger" style="font-size: small; background-color: #D74449;"><i
-              class="fas fa-shopping-cart"></i>&#160;Đặt ngay</a>
-          <a href="#" class="btn btn-light" style="font-size: small; background-color: #ffffff; float: right; border: 1px solid #2f24a7; color: #2f24a7;">Xem chi tiết</a>
-        </div>
-      </div>
+      <?php
+        $sql = "select count(tours.tour_id) as count, tours.tour_id, tours.tour_name, tours.price_per_person, tours.day_count, cities.city_name, tours.image_1, ranks.rank_name from tours, orders, cities, ranks where tours.tour_id = orders.tour_id and tours.city_id = cities.city_id and tours.tour_rank_id = ranks.rank_id group by tours.tour_id ORDER BY count DESC LIMIT 3";
+        $tour = simpleQuery($sql);
+        foreach($tour as $t){
+          $price = $t["price_per_person"];
+          $price = number_format($price, 0, "", ",");
+          echo '
+            <div class="card uu-dai">
+              <div class="h-card-container">
+                <a href="#">
+                  <img src="'.$t["image_1"].'" class="card-img-top h-latest-tour-image" alt="...">
+                </a>
+                <div class="h-card-rank">'.$t["rank_name"].'</div>
+              </div>
+              <div class="card-body">
+                <p class="card-text mb-1"><small class="text-muted">28/10/2021 - '.$t["day_count"].' ngày</small></p>
+                <a href="#"><h5 class="card-title pr-2 mb-1" style="color: #282365; font-size: medium;">'.$t["tour_name"].'</h5></a>
+                <!-- <p class="card-text mb-0"><small class="text-muted">Mã tour:</small></p> --!>
+                <!-- <p class="card-text mb-2"><small class="text-muted"><i class="fas fa-ticket"></i>
+                    NDSGN8286-001-281021XE-V</small></p> --!>
+                <!-- <p class="card-text"><small class="text-muted">Nơi khởi hành TP.Hồ Chí Minh</small></p> --!>
+                <p class="card-text"><b style="font-size: large; color:#FD5056;">'.$price.'đ</b><small class="text-muted">/người</small></p>
+                <a href="#" class="btn btn-danger" style="font-size: small; background-color: #D74449;"><i
+                    class="fas fa-shopping-cart"></i>&#160;Đặt ngay</a>
+                <a href="#" class="btn btn-light" style="font-size: small; background-color: #ffffff; float: right; border: 1px solid #2f24a7; color: #2f24a7;">Xem chi tiết</a>
+              </div>
+            </div>
+          ';
+        }
+      ?>
     </div>
     <br>
     <button type="button" class="btn btn-light pl-4 pr-4"
