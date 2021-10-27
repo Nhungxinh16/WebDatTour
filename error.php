76d49ad5a6c42a284875063e7e92c9efd165072c
Cong <congvip17a17@gmail.com>
2021-10-27 15:45:43 +0700

    add error

---
+++ error.php
@@ -0,0 +1,39 @@
+<?php
+   /* require("config/constants.php"); */ 
+    require('templates/header.php');
+?>
+
+<body class="app" cz-shortcut-listen="true">
+  <div class="container">
+      <div class="pos-a t-0 l-0 bgc-white w-100 h-100 d-f fxd-r fxw-w ai-c jc-c pos-r p-30">
+        <div class="mR-60"><img alt="#" src="content/image/404.png"></div>
+        <div class="d-f jc-c fxd-c">
+          <h1 class="mB-30 fw-900 lh-1 c-red-500" style="font-size:60px">404</h1>
+          <h3 class="mB-10 fsz-lg c-grey-900 tt-c">Lỗi</h3>
+          <p class="mB-30 fsz-def c-grey-700">Trang bạn đang tìm kiếm không tồn tại hoặc đã bị di chuyển.</p>
+          <div>
+            <a href="index.php" type="primary" class="btn btn-primary">Quay lại trang chủ</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <hr>
+  <footer>
+    <div class="row justify-content-lg-around mx-xl-5 mx-lg-4 mx-3 py-3">
+      <div class="col order-1 align-self-center ml-4">
+        <small>Bản quyền của Vietravel ® 2016. Bảo lưu mọi quyền. <br>
+          Ghi rõ nguồn "www.travel.com.vn" ® khi sử dụng lại thông tin từ website này. <br>
+          Số giấy phép kinh doanh lữ hành Quốc tế: 79-234/2014/TCDL-GP LHQT</small>
+      </div>
+      <div class="col-auto my-sm-0 order-6 my-auto pt-sm-0 pt-3">
+        <ul class="list-inline third-row first">
+          <li class="list-inline-item active-1 "> <span></span> </li>
+          <li class="list-inline-item"><span></span></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
+
