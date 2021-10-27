979785b68ac393e8b5e1efa97b77525ca21603f4
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2021-10-27 21:47:15 +0700

    lam chuc nang them tour cua admin

--- admin/index.php
+++ admin/index.php
@@ -3,11 +3,11 @@ nó đang ở chỗ quản lý tour
 -->
 
 <?php
-/*     require("config/constants.php"); */
+    require_once("../config/constants.php");
     require('templates-admin/header.php');
 ?>
 
-            <div class="container-fluid px-4">
+            <div class="container-fluid px-4" style="margin-bottom: 20%;">
 
                 <div class="row my-5">
                     <h3 class="fs-4 mb-3">Quản lý tour</h3>
@@ -17,50 +17,46 @@ nó đang ở chỗ quản lý tour
                                 <tr>
                                     <th scope="col" width="50">#</th>
                                     <th scope="col">Tên tour</th>
-                                    <th scope="col">Địa chỉ</th>
+                                    <th scope="col">Số ngày</th>
                                     <th scope="col">Giá</th>
-                                    <th scope="col">Birthday</th>
-                                    <th scope="col">Điện thoại</th>
-                                    <th scope="col">Họ tên</th>
-                                    <th scope="col">Giới tính</th>
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Price</th>                                    
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Price</th>
-                                    <th scope="col">Price</th>
+                                    <th scope="col">Thành phố</th>
+                                    <th scope="col">Loại tour</th>
+                                    <th scope="col">Hạng</th>
+                                    <th scope="col">Xem chi tiết</th>
+                                    <th scope="col">Sửa</th>
+                                    <th scope="col">Xóa</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Television</td>
-                                    <td>Jonny</td>
-                                    <td>$1200</td>
-                                    <td>Television</td>
-                                    <td>Jonny</td>
-                                    <td>$1200</td>
-                                    <td>Television</td>
-                                    <td>Jonny</td>
-                                    <td>$1200</td>
-                                    <td><a href="#">Xem chi tiết</a></td>
-                                    <td><a href="sua-tour.php">Sửa</a></td>
-                                    <td><a href="#">xóa</a></td>
-                                </tr>
+                                <?php
+                                    $sql = "select * from tours, cities, tourtypes, ranks where tours.city_id = cities.city_id and tours.tour_rank_id = ranks.rank_id and tours.tour_type_id and tourtypes.tour_type_id group by tours.tour_id order by tours.tour_id desc";
+                                    $tours = simpleQuery($sql);
+                                    $i = 1;
+                                    foreach($tours as $t){
+                                        echo '
+                                            <tr>
+                                                <th scope="row">'.$i.'</th>
+                                                <td>'.$t["tour_name"].'</td>
+                                                <td>'.$t["day_count"].'</td>
+                                                <td>'.$t["price_per_person"].'đ</td>
+                                                <td>'.$t["city_name"].'</td>
+                                                <td>Tour '.$t["type_name"].'</td>
+                                                <td>'.$t["rank_name"].'</td>
+                                                <td><a href="../more_info.php?tour_id='.$t["tour_id"].'"><i class="fas fa-eye"></i></a></td>
+                                                <td><a href="sua-tour.php"><i class="fas fa-edit"></i></a></td>
+                                                <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                        ';
+                                        $i++;
+                                    }
+
+                                ?>
                             </tbody>
                         </table>
                     </div>
                 </div>
                 <button type="button" class="btn btn-primary" style="margin-bottom: 12px"><a href="them-tour.php" class="text-white">Thêm</a></button>
 
-          <div>
-            <form action="export_import.php" method="POST" enctype="multipart/form-data">
-                <input type="file" name="file_import">
-                <button class="btn btn-dark" type="submit" name="import">IMPORT</button>
-            </form>
-            <form action="export_import.php" method="POST" enctype="multipart/form-data">
-                <button class="btn btn-dark" type="submit" name="export">EXPORT</button>
-            </form>
-          </div>
 
 <?php
     include('templates-admin/footer.php')
--- admin/them-tour.php
+++ admin/them-tour.php
@@ -1,7 +1,31 @@
 <!-- trang này dùng để thêm các tour -->
-
 <?php
-include('templates-admin/header.php');
+    require_once("../config/constants.php");
+    if(isset($_POST["submit"])){
+        $tour_name = $_POST["tour_name"];
+        $description = $_POST["description"];
+        $places = $_POST["places"];
+        $day_count = $_POST["day_count"];
+        $price_per_person = $_POST["price_per_person"];
+        $max = $_POST["max"];
+        $image_1 = $_POST["image_1"];
+        $image_2 = $_POST["image_2"];
+        $image_3 = $_POST["image_3"];
+        $image_4 = $_POST["image_4"];
+        $travel_schedule = $_POST["travel_schedule"];
+        $res_id = $_POST["res_id"];
+        $hotel_id = $_POST["hotel_id"];
+        $city_id = $_POST["city_id"];
+        $tour_guide_id = $_POST["tour_guide_id"];
+        $tour_type_id = $_POST["tour_type_id"];
+        $tour_rank_id = $_POST["tour_rank_id"];
+        $sql = "insert into tours set tour_name = ?, description = ?, places = ?, day_count = ?, res_id = ?, hotel_id = ?, price_per_person = ?, image_1 =?, image_2 =?, image_3 =?, image_4 = ?, city_id =?, tour_guide_id = ?, tour_type_id = ?, tour_rank_id = ?, travel_schedule = ?, max = ?";
+        $result = simpleQuery($sql, 0, [$tour_name, $description, $places, $day_count, $res_id, $hotel_id, $price_per_person, $image_1, $image_2, $image_3, $image_4, $city_id, $tour_guide_id, $tour_type_id, $tour_rank_id, $travel_schedule, $max]);
+        if($result){
+          header("location: index.php");
+        }
+    }
+    include('templates-admin/header.php');
 ?>
 
 <div class="main-content">
@@ -11,45 +35,139 @@ include('templates-admin/header.php');
             <div class="text-center text-danger">
             </div>
         </div>
-  <!-- sửa -->
+  <!-- Thêm -->
         <div class="container col-md-12 mx-auto">
-            <form action="process-add.php" METHOD="POST">
+            <form style="margin-bottom: 20%;" action="them-tour.php" METHOD="POST">
                 <div class="col-md-6 mx-auto">
                     <div class="input-group mb-2">
                         <span class="input-group-text col-3">Tên tour</span>
-                        <input type="text" class="form-control" name= "txthoten" placeholder="Nhập tên tour">
+                        <input type="text" class="form-control" name= "tour_name" placeholder="Nhập tên tour" required>
                     </div>
-
                     <div class="input-group mb-2">
-                        <span class="input-group-text col-3" >Chức Vụ</span>
-                        <input type="text" class="form-control" name= "txtchucvu" placeholder="Nhập chức vụ">
+                        <span class="input-group-text col-3" >Mô tả</span>
+                        <input type="text" class="form-control" name= "description" placeholder="Nhập mô tả">
                     </div>
                     
                     <div class="input-group mb-2">
-                        <span class="input-group-text col-3" >Máy bàn</span>
-                        <input type="text" class="form-control" name= "txtmayban" placeholder="Nhập số máy bàn">
+                        <span class="input-group-text col-3" >Các điểm đến</span>
+                        <input type="text" class="form-control" name= "places" placeholder="Nhập các điểm đến" required>
                     </div>
 
                     <div class="input-group mb-2"> 
-                        <span class="input-group-text col-3" >Email</span>
-                        <input type="email" class="form-control" name="txtemail" placeholder="Nhập email" >      
+                        <span class="input-group-text col-3" >Số ngày</span>
+                        <input type="number"  min = "1" max = "15" class="form-control" name="day_count" placeholder="Nhập số ngày" required>      
                     </div>
 
                     <div class="input-group mb-2"> 
-                        <span class="input-group-text col-3" >Số điện thoại</span>
-                        <input type="tel" class="form-control" name="sodidong" placeholder="Nhập số điện thoại">             
+                        <span class="input-group-text col-3" >Số người tối đa</span>
+                        <input type="number"  min = "1" max = "15" class="form-control" name="max" placeholder="Nhập số người tối đa" required>      
+                    </div>
+
+                    <div class="input-group mb-2"> 
+                        <span class="input-group-text col-3" >Giá tour</span>
+                        <input type="number"  min = "100000" max = "100000000" class="form-control" name="price_per_person" placeholder="Nhập giá tour" required>      
+                    </div>
+
+                    <div class="input-group mb-2"> 
+                        <span class="input-group-text col-3" >Hình ảnh 1</span>
+                        <input type="text" class="form-control" name="image_1" placeholder="Link hình 1" required>             
+                    </div>
+
+                    <div class="input-group mb-2"> 
+                        <span class="input-group-text col-3" >Hình ảnh 2</span>
+                        <input type="text" class="form-control" name="image_2" placeholder="Link hình 2" required>             
+                    </div>
+
+                    <div class="input-group mb-2"> 
+                        <span class="input-group-text col-3" >Hình ảnh 3</span>
+                        <input type="text" class="form-control" name="image_3" placeholder="Link hình 3" required>             
+                    </div>
+
+                    <div class="input-group mb-2"> 
+                        <span class="input-group-text col-3" >Hình ảnh 4</span>
+                        <input type="text" class="form-control" name="image_4" placeholder="Link hình 4" required>             
+                    </div>
+                    
+                    <div class="input-group mb-2"> 
+                        <span class="input-group-text col-3" >Lịch trình</span>
+                        <textarea class="form-control" name="travel_schedule" cols="30" rows="10"></textarea>
                     </div>
 
                     <div class="input-group mb-2 ">
-                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên Đơn Vị</label>
-                        <select class="form-select form-select-sm" name="sltMaDV" id="sltMaDV">
+                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên nhà hàng</label>
+                        <select class="form-select form-select-sm" name="res_id">
+                            <?php 
+                                $sql = "select * from restaurants";
+                                $rows = simpleQuery($sql);
+                                foreach($rows as $row){
+                                    echo '<option value="'.$row["res_id"].'">'.$row["res_name"].'</option>';
+                                }
+                            ?>
                         </select>
                     </div>
-                    <button type="submit" class="btn btn-info" name="submit"><a href="index.php">Thêm</a></button>
-     
+                    <div class="input-group mb-2 ">
+                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên khách sạn</label>
+                        <select class="form-select form-select-sm" name="hotel_id">
+                            <?php 
+                                $sql = "select * from hotels";
+                                $rows = simpleQuery($sql);
+                                foreach($rows as $row){
+                                    echo '<option value="'.$row["hotel_id"].'">'.$row["hotel_name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="input-group mb-2 ">
+                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên thành phố</label>
+                        <select class="form-select form-select-sm" name="city_id" required>
+                            <?php 
+                                $sql = "select * from cities";
+                                $rows = simpleQuery($sql);
+                                foreach($rows as $row){
+                                    echo '<option value="'.$row["city_id"].'">'.$row["city_name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="input-group mb-2 ">
+                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên hướng dẫn viên</label>
+                        <select class="form-select form-select-sm" name="tour_guide_id">
+                            <?php 
+                                $sql = "select * from tourguides";
+                                $rows = simpleQuery($sql);
+                                foreach($rows as $row){
+                                    echo '<option value="'.$row["tour_guide_id"].'">'.$row["name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="input-group mb-2 ">
+                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên loại tour</label>
+                        <select class="form-select form-select-sm" name="tour_type_id" required>
+                            <?php 
+                                $sql = "select * from tourtypes";
+                                $rows = simpleQuery($sql);
+                                foreach($rows as $row){
+                                    echo '<option value="'.$row["tour_type_id"].'">'.$row["type_name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="input-group mb-2 ">
+                        <label class="input-group-text col-3" for="inputGroupSelect01">Tên hạng</label>
+                        <select class="form-select form-select-sm" name="tour_rank_id" required>
+                            <?php 
+                                $sql = "select * from ranks";
+                                $rows = simpleQuery($sql);
+                                foreach($rows as $row){
+                                    echo '<option value="'.$row["rank_id"].'">'.$row["rank_name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-info" name="submit">Thêm</button>
                 </div>
             </form>
-                      
         </div>        
     </div>
 </div>
