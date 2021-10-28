a1db261d20e65071af265bdaa3e605381c8ff43c
Nhungxinh16 <tuyetnhung01062001@gmail.com>
2021-10-28 11:22:22 +0700

    them chuc nang o QL dat tour

--- admin/QLDDTour.php
+++ admin/QLDDTour.php
@@ -1,16 +1,75 @@
 <!-- trang này là khi bấm vào quản lý đặt tour trên nav bar sẽ ra 
 nó dùng để quản lý đặt tour -->
 
-
 <?php
-/*     require("config/constants.php"); */
+    require("../config/constants.php");
+    // Hủy các orders mà 2 ngày chưa thanh toán
+    $now = date("Y-m-d");
+    $last_time = (int) strtotime($now) - (int) 2 * 86400;
+    $last_time = date_create(date("Y-m-d", $last_time));
+    $last_time = date_format($last_time, "Ymd");
+    $start_time = (int) strtotime($now) - (int) 4 * 86400;
+    $start_time = date_create(date("Y-m-d", $start_time));
+    $start_time = date_format($start_time, "Ymd");
+    $sql = "SELECT * FROM orders WHERE date_created < ? and date_created > ? and payment = 0";
+    $orders = simpleQuery($sql, 1, [$last_time, $start_time]);
+    $sql = "update orders set validation = ?, payment = ?";
+    foreach($orders as $order){
+        simpleQuery($sql, 0, [1,2]);
+    }
+
+    if(isset($_GET["order_id"]) && $_GET["order_id"] != null){
+        $id = $_GET["order_id"];
+        $sql = "update orders set validation = ? where order_id = ?";
+        $result = false;
+        require_once("../config/mail.php");
+        if(isset($_GET["val"])){
+            $result = simpleQuery($sql, 0, [1, $id]);
+            if($result){
+                $subject = 'MyTour kính chào quý khách'; 
+                $bodyContent = '
+                    <p>Cảm ơn quý khách đã đăng kí tour trên MyTour.</p>
+                    <p>Xin quý khách vui lòng vào đường <a href="http://localhost:88/WebDatTour/pay-booking.php?order_id='.$id.'">link</a> 
+                    này để có thể thanh toán tour. Thời hạn thanh toán
+                    là 2 ngày kể từ ngày quý khách đăng kí. Nếu quý khách không thanh toán đúng hạn, thì 
+                    yêu cầu đặt tour của quý khách sẽ bị hủy bỏ.</p>
+                    <p>Kính mến!</p>
+                ';
+                send_mail("tuyetnhung01062001@gmail.com", $subject, $bodyContent);
+                header("location: QLDDTour.php");
+            }
+        }else if(isset($_GET["cancel"])){
+            $result = simpleQuery($sql, 0, [2, $id]);
+            if($result){
+                $subject = 'MyTour: hủy tour du lịch'; 
+                $bodyContent = '
+                    <p>Cảm ơn quý khách đã tin tưởng dịch vụ của MyTour. 
+                    Tour của quý khách đã bị chúng tôi hủy. Nếu không phải do bạn yêu cầu hủy tour thì có thể là một trong những
+                    lý do sau: </p>
+                    <ul>
+                        <li>MyTour không tin tưởng quý khách</li>
+                        <li>Không có tour nào khả dụng</li>
+                        <li>MyTour chưa sẵn sàng phục vụ quý khách </li>
+                    </ul>
+                    <p> Khi nhận được mail này mà không phải bạn yêu cầu hủy tour bạn có thể đăng ký tour lại, chúng tôi
+                    tin rằng yêu cầu đặt tour của quý khách sẽ chấp nhận cao hơn.
+                    </p>
+                    <p>Kính mến!</p>
+                ';
+                send_mail("tuyetnhung01062001@gmail.com", $subject, $bodyContent);
+                header("location: QLDDTour.php");
+            }
+        }   
+    }
+    
+
     require('templates-admin/header.php');
 ?>
 
             <div class="container-fluid px-4">
 
                 <div class="row my-5">
-                    <h3 class="fs-4 mb-3">Quản lý đơn đặt tour</h3>
+                    <h3 class="fs-4 mb-3">Các tour đang chờ xử lý</h3>
                     <div class="col">
                         <table class="table bg-white rounded shadow-sm  table-hover">
                             <thead>
@@ -21,27 +80,135 @@ nó dùng để quản lý đặt tour -->
                                     <th scope="col">Thời gian bắt đầu</th>
                                     <th scope="col">Số người</th>
                                     <th scope="col">Số ngày</th>
-                                    <th scope="col">Xem chi tiết</th>
-                                    <th scope="col">Sửa</th>
-                                    <th scope="col">Xóa</th>
+                                    <th scope="col">Xác nhận</th>
+                                    <th scope="col">Hủy bỏ</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Television</td>
-                                    <td>Jonny</td>
-                                    <td>$1200</td>
-                                    <td>Television</td>
-                                    <td>Jonny</td>
-                                    <td><a href="#">Xem chi tiết</a></td>
-                                    <td><a href="#"><i class="fas fa-edit"></i></a></td>
-                                    <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
-                                </tr>
+                                <?php
+                                    $sql = "select * from orders, tours, customers where orders.customer_id = customers.customer_id and orders.tour_id = tours.tour_id and orders.validation = 0";
+                                    $rows = simpleQuery($sql);
+                                    $i = 1;
+                                    foreach($rows as $row){
+                                        echo '
+                                            <tr>
+                                                <th scope="row">'.$i.'</th>
+                                                <td>'.$row["user_name"].'</td>
+                                                <td>'.$row["tour_name"].'</td>
+                                                <td>'.$row["start_time"].'</td>
+                                                <td>'.$row["people"].'</td>
+                                                <td>'.$row["day_count"].'</td>
+                                                <td><a href="QLDDTour.php?val=&order_id='.$row["order_id"].'" id="check_tick"><i class="fas fa-check-circle"></i></a></td>
+                                                <td><a href="QLDDTour.php?cancel=&order_id='.$row["order_id"].'"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                        ';
+                                        $i++;
+                                    }
+                                    
+                                ?>
+                                
                             </tbody>
                         </table>
                     </div>
-                </div>             
+                </div>     
+                <div class="row my-5">
+                    <h3 class="fs-4 mb-3">Danh sách chờ thanh toán</h3>
+                    <div class="col">
+                        <table class="table bg-white rounded shadow-sm  table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col" width="50">#</th>
+                                    <th scope="col">Người đặt tour</th>
+                                    <th scope="col">Tên tour</th>
+                                    <th scope="col">Thời gian bắt đầu</th>
+                                    <th scope="col">Số người</th>
+                                    <th scope="col">Số ngày</th>
+                                    <th scope="col">Tình trạng</th>
+                                    <th scope="col">Hủy bỏ</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $sql = "select * from orders, tours, customers where orders.customer_id = customers.customer_id and orders.tour_id = tours.tour_id and orders.validation = 1 and orders.payment = 0 order by orders.order_id desc";
+                                    $rows = simpleQuery($sql);
+                                    $i = 1;
+                                    foreach($rows as $row){
+                                        echo '
+                                            <tr>
+                                                <th scope="row">'.$i.'</th>
+                                                <td>'.$row["user_name"].'</td>
+                                                <td>'.$row["tour_name"].'</td>
+                                                <td>'.$row["start_time"].'</td>
+                                                <td>'.$row["people"].'</td>
+                                                <td>'.$row["day_count"].'</td>
+                                                <td>Chờ thanh toán</td>
+                                                <td><a href="QLDDTour.php?cancel=&order_id='.$row["order_id"].'"><i class="fas fa-trash-alt"></i></a></td>
+                                            </tr>
+                                        ';
+                                        $i++;
+                                    }
+                                    
+                                ?>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>  
+                <div class="row my-5">
+                    <h3 class="fs-4 mb-3">Danh sách các tour còn lại</h3>
+                    <div class="col">
+                        <table class="table bg-white rounded shadow-sm  table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col" width="50">#</th>
+                                    <th scope="col">Người đặt tour</th>
+                                    <th scope="col">Tên tour</th>
+                                    <th scope="col">Thời gian bắt đầu</th>
+                                    <th scope="col">Số người</th>
+                                    <th scope="col">Số ngày</th>
+                                    <th scope="col">Tình trạng</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $sql = "select * from orders, tours, customers where orders.customer_id = customers.customer_id and orders.tour_id = tours.tour_id and orders.validation > 0 and orders.payment > 0 order by orders.order_id desc";
+                                    $rows = simpleQuery($sql);
+                                    $i = 1;
+                                    foreach($rows as $row){
+                                        echo '
+                                            <tr>
+                                                <th scope="row">'.$i.'</th>
+                                                <td>'.$row["user_name"].'</td>
+                                                <td>'.$row["tour_name"].'</td>
+                                                <td>'.$row["start_time"].'</td>
+                                                <td>'.$row["people"].'</td>
+                                                <td>'.$row["day_count"].'</td>
+                                                <td>'; 
+                                                    if($row["validation"] == 0){
+                                                        echo "Đang chờ mytour xử lý";
+                                                    }else if($row["validation"] == 2){
+                                                        echo "Đã bị mytour hủy";
+                                                    }else{
+                                                        if($row["payment"] == 0){
+                                                            echo "Chưa thanh toán";
+                                                        }else if($row["payment"] == 1){
+                                                            echo "Đã thanh toán";
+                                                        }else{
+                                                            echo "Bị hủy bỏ vì chưa thanh toán";
+                                                        }
+                                                    }
+                                                echo '</td>
+                                            </tr>
+                                        ';
+                                        $i++;
+                                    }
+                                    
+                                ?>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>               
 
 <?php
     include('templates-admin/footer.php')
--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,5 +1,6 @@
 <?php
     require_once("../config/constants.php");
+
     if(isset($_POST["mode"])){
         $mode = $_POST["mode"];
         if($mode == "order"){
@@ -17,8 +18,30 @@
         }else if($mode == "payment"){
             $order_id = $_POST["order_id"];
             $sql = "update orders set payment = 1 where order_id = ?";
+            $result = true;
             $result = simpleQuery($sql, 0, [$order_id]);
+           
             if($result){
+                require_once("../config/mail.php");
+                $sql = "select * from tours, cities, orders where orders.tour_id = tours.tour_id and tours.city_id = cities.city_id and orders.order_id = ?";
+                $row = simpleQuery($sql, 1, [$order_id])[0];
+                $date = date_create($row["start_time"]);
+                $day = date_format($date, "d");
+                $month = date_format($date, "m");
+                $year = date_format($date, "Y");
+                $paySuccess = '
+                    <p>Chào bạn</p>
+                    <p>Bạn đã thanh toán thành công hãy chuẩn bị đồ đạc và hẹn gặp tại '.$row["city_name"].' vào 
+                    ngày '.$day.' tháng '.$month.' năm '.$year.'. Nếu quý khách không có mặt thì chúng tôi rất 
+                    xin lỗi vì chúng tôi sẽ hủy tour của bạn và chúng tôi không thể hoàn tiền lại cho quý khách. Các bạn có thể liên hệ để hủy tour nếu đây không phải tour do bạn đặt hoặc do bạn đặt nhầm.
+                    </p>
+                    <p>Qúy khách muốn xem thông tin chi tiết về hành trình đầy khó khăn của mình thì có thể truy cập vào 
+                    link sau: <a href="http://localhost:88/WebDatTour/history.php?customer_id='.$row["customer_id"].'">Chi tiết</a>
+                    </p>
+                    <p>Kính chúc quý khách đi chơi vui vẻ</p>
+                ';
+                send_mail("tuyetnhung01062001@gmail.com", "myTour: Thanh toán thành công", $paySuccess);
+                // echo $paySuccess;
                 echo "ok";
             }else{
                 echo "no";
